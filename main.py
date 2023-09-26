311c9c9e081e5f2925c2773debcd956309ddd2a9
cheongy <cheongy@educ.college-montreal.qc.ca>
2023-09-26 12:08:57 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#variables
+essai = int(input("entrez un chiffre de 0 à 1000\n"))
+nombre = import random
+         random.randint(0,1000)
+
+
+#programme
+if (essai < nombre)
+    print("Mauvaise réponse, le nombre est plus grand que essai\n entrez votre essai")
+
+elif (essai > nombre)
+    print("")
