5c17e143841cab96acdc068b85ad74eef1076026
cheongy <cheongy@educ.college-montreal.qc.ca>
2023-09-28 12:04:38 -0400

    drttykdykt

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 #variables
-essai = int(input("entrez un chiffre de 0 à 1000\n"))
-nombre = import random
-         random.randint(0,1000)
-
+import random
+#borne_minimale = int(input("entrez la borne minimale\n"))
+#borne_maximale = int(input("entrez la borne maximale\n"))
+#nombre = random.randint(borne_minimale,borne_maximale)
+#essai = int(input("entrez un chiffre de " + str(borne_minimale) +" à "+ str(borne_maximale) +"\n"))
+#nbessai = 0
+reponse = 'oui'
 
 #programme
-if (essai < nombre)
-    print("Mauvaise réponse, le nombre est plus grand que essai\n entrez votre essai")
+while reponse == "oui":
+    borne_minimale = int(input("entrez la borne minimale\n"))
+    borne_maximale = int(input("entrez la borne maximale\n"))
+    nombre = random.randint(borne_minimale, borne_maximale)
+    essai = int(input("entrez un chiffre de " + str(borne_minimale) + " à " + str(borne_maximale) + "\n"))
+    nbessai = 0
+    while essai != nombre:
+        nbessai += 1
+        if essai < nombre:
+            print("Mauvaise réponse, le nombre est plus grand que essai\n entrez votre essai")
 
-elif (essai > nombre)
-    print("")
+        elif essai > nombre:
+            print("Mauvaise réponse, le nombre est plus petit que essai\n entrez votre essai")
+
+        essai = int(input("entrez un chiffre de " + str(borne_minimale) + " à " + str(borne_maximale) + "\n"))
+
+    nbessai += 1
+    print("Bonne réponse vous avez deviné en "+ str(nbessai) +" essais")
+    reponse = str(input("voulez-vous faire une autre partie?\n oui ou non\n"))
+
+
+    if reponse =="non":
+        exit()
+
