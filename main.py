016ea75b03a410974f29b281a955a77a33754e30
clicmaths <144700969+clicmaths@users.noreply.github.com>
2023-10-24 11:26:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import random
 reponse = 'oui'
 
 
-#programme
-while reponse == "oui":#boucle pour rejouer
+#boucle pour rejouer
+while reponse == "oui":
     def bornes():#fonctions pour choisir la borne minimale et maximale
         borne_minimale = int(input("entrez la borne minimale\n"))
         borne_maximale = int(input("entrez la borne maximale\n"))
         return(borne_minimale, borne_maximale)
 
-
-    (borne_minimale, borne_maximale) = bornes() #définitions des variables
-    nombre = random.randint(borne_minimale, borne_maximale) # importer une valeur aléatoire
+    #définitions des variables
+    (borne_minimale, borne_maximale) = bornes() 
+    # importer une valeur aléatoire
+    nombre = random.randint(borne_minimale, borne_maximale) 
     essai = int(input("entrez un chiffre de " + str(borne_minimale) + " à " + str(borne_maximale) + "\n"))
     nbessai = 0
-    while essai != nombre:#boucle pour dire si l'utilisateur a écris la bonne réponse
+    #boucle pour dire si l'utilisateur a écris la bonne réponse
+    while essai != nombre:
         nbessai += 1
         if essai < nombre:
             print("Mauvaise réponse, le nombre est plus grand que essai\n entrez votre essai")
